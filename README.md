6cc517b0ab8c69e9f60534be1a06140902a98b9c
marhaupe <marhaupe97@gmail.com>
2020-10-02 17:10:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# japityper
+
+Makes typing glue responses easier
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/japityper.svg)](https://npmjs.org/package/japityper)
+[![Downloads/week](https://img.shields.io/npm/dw/japityper.svg)](https://npmjs.org/package/japityper)
+[![License](https://img.shields.io/npm/l/japityper.svg)](https://github.com/marhaupe/japityper/blob/master/package.json)
+
+<!-- toc -->
+* [japityper](#japityper)
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+
+# Usage
+
+<!-- usage -->
+```sh-session
+$ npm install -g japityper
+$ japityper COMMAND
+running command...
+$ japityper (-v|--version|version)
+japityper/0.0.1 darwin-x64 node-v12.16.1
+$ japityper --help [COMMAND]
+USAGE
+  $ japityper COMMAND
+...
+```
+<!-- usagestop -->
+
+# Commands
+
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/index.ts
@@ -0,0 +1,110 @@
+import * as fs from "fs";
+import * as clipboardy from "clipboardy";
+import { Deserializer } from "ts-jsonapi";
+import { Command, flags } from "@oclif/command";
+import {
+  quicktype,
+  InputData,
+  jsonInputForTargetLanguage,
+} from "quicktype-core";
+
+// Quicktype doesn't let you opt out of generating some code,
+// so we have to do that ourselves.
+function removeUnwantedLines(lines: string[]): string[] {
+  const strippedLines = [...lines];
+
+  // Delete the first few lines that are only comments
+  strippedLines.splice(
+    0,
+    strippedLines.findIndex((line) => !line.startsWith("//"))
+  );
+
+  // The second half of the generated code consists of a Converter class.
+  // This is only useful for certain output languages, e.g. TypeScript.
+  strippedLines.splice(
+    strippedLines.indexOf("// Converts JSON strings to/from your types")
+  );
+  return strippedLines;
+}
+
+class Japityper extends Command {
+  static description =
+    "reads JSON:API content from your clipboard and generates types for the deserialized content";
+
+  static flags = {
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    language: flags.string({
+      char: "l",
+      default: "ts",
+      description: "language of generated code",
+    }),
+    "root-type": flags.string({
+      char: "r",
+      description: "root type name",
+      default: "Root",
+    }),
+  };
+
+  static args = [
+    {
+      name: "file",
+      required: false,
+      description: "output file",
+    },
+  ];
+
+  async run() {
+    const { args, flags } = this.parse(Japityper);
+    let clipboardContent = clipboardy.readSync();
+    clipboardContent = clipboardContent.trimStart();
+
+    let response: any | null = null;
+    try {
+      response = JSON.parse(clipboardContent);
+    } catch (error) {
+      this.error("parsing clipboard content failed. " + error.message);
+    }
+
+    const deserializer = new Deserializer({
+      keyForAttribute: (key: string) => key,
+    });
+
+    let deserializedInput: any | null = null;
+    try {
+      deserializedInput = deserializer.deserialize(response);
+    } catch (error) {
+      this.error("deserializing failed. " + error.message);
+    }
+
+    const jsonInput = jsonInputForTargetLanguage(flags.language);
+
+    await jsonInput.addSource({
+      name: flags["root-type"],
+      samples: [JSON.stringify(deserializedInput)],
+    });
+
+    const inputData = new InputData();
+    inputData.addInput(jsonInput);
+
+    let { lines } = await quicktype({
+      inputData,
+      lang: flags.language,
+      allPropertiesOptional: true,
+      combineClasses: true,
+      inferMaps: true,
+      inferEnums: true,
+    });
+
+    lines = removeUnwantedLines(lines);
+
+    const output = lines.join("\n");
+    if (args.file) {
+      fs.writeFileSync(args.file, output);
+    } else {
+      this.log(output);
+    }
+  }
+}
+
+export = Japityper;
